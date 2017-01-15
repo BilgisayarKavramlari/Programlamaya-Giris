8e943c9b30d45bdde6b90e83881e6cc844afbea8
kaganuk <kagan@Kagan-MacBook-Pro.local>
2017-01-16 01:38:39 +0300

    Swift İle Döngü Örneği

---
+++ Dongu13Katlari.swift
@@ -0,0 +1,12 @@
+//  Created by Kağan on 15.01.2017.
+//  Copyright © 2017 Kağan Utku Kılıçlı. All rights reserved.
+
+import Foundation
+
+for index in stride(from: 100, to: 0, by: -1) {
+    if(index % 13) == 0 {
+        print(index)
+    }
+}
+
+

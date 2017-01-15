3725d1896799e388559a3de44f2e1532caaf0b8c
Ebubekir Karanfil <ebu.bjk@gmail.com>
2017-01-15 12:57:26 +0200

    Java Dilinde Çarpım Tablosu
    Ebubekir Karanfil tarafından kodlanmıştır.

---
+++ CarpimTablosu.java
@@ -0,0 +1,19 @@
+
+package carpimtablosu;
+public class carpimTablosu {
+
+    
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=10;i++)
+        {
+            for(int b=1;b<=10;b++)
+            {
+                System.out.println(i + " X " + b +" = " + (i*b));
+            }
+            System.out.println("*******");
+        }
+       
+    }
+    
+}

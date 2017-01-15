0238beab5a8df4c36abc48e4248abb81d2716266
Osmanyasal <osman.yasal@ogr.sakarya.edu.tr>
2017-01-15 12:52:40 +0200

    SayiOkuma.cs
    Osman Yasal tarafından kodlanmıştır.

---
+++ SayiOkuma.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayi giriniz :");
+
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Girilen Sayi :");
+
+            Console.WriteLine(x);
+
+             
+
+        }
+    }
+}

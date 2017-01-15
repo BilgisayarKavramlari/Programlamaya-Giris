b7e81960cd14e1ec8310dc91325233286a18dd0d
Özcan Yarımdunya <ozcanyd@gmail.com>
2017-01-15 15:03:53 +0200

    3 sayının en büyük ve en küçüğünü bulma programı
    http://semiworld.org

---
+++ 3SayininEnBuyukEnKucugu.py
@@ -0,0 +1,15 @@
+x = int(input('İlk sayıyı giriniz: '))
+y = int(input('İkinci sayıyı giriniz: '))
+z = int(input('Üçüncü sayıyı giriniz: '))
+
+eb = x
+ek = x
+for n in x,y,z:
+    if n > eb:
+        eb = n
+    if n < ek:
+        ek = n
+
+print('''
+    En büyük : {}
+    En küçük : {}'''.format(eb,ek))

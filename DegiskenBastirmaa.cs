98957ce7a986a1b876eae58c17ff2a9808b19f45
Osmanyasal <osman.yasal@ogr.sakarya.edu.tr>
2017-01-15 12:49:54 +0200

    DegiskenBastirmaa.cs
    Osman Yasal tarafından kodlanmıştır.

---
+++ DegiskenBastirmaa.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+
+            Console.WriteLine(x);
+        }
+    }
+}

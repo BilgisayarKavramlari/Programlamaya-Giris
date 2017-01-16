039e3d251883f0b6c1fd74a3791e0e9d0898c1a6
Tolga Merit Tekin <tolgamerit@gmail.com>
2017-01-16 04:29:34 +0200

    JAVA 1 den 10'a Kadar Ardışık Döngü

---
+++ ArdisikDongu.java
@@ -0,0 +1,11 @@
+package ardisikdongu;
+
+public class ArdisikDongu {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}

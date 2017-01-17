4ff5573adc2d443d6fbfbcd12e67af089784f43f
Feyyaz ONUR <feyyazonur0@gmail.com>
2017-01-17 13:22:55 +0200

    Python 3.x dilince İşçi Problemi Çözümü
    #feyyazonur

---
+++ IsciProblemi.py
@@ -0,0 +1,8 @@
+birIsciBitirmeS = int(input("Bir işçi işi kaç günde bitirmektedir? "))
+toplamIsci = int(input("Toplam kaç işçi çalışacak? "))
+
+IsinBitmeS = birIsciBitirmeS / toplamIsci
+
+print("İşin bitme süresi",IsinBitmeS,"gündür." )
+
+#feyyazonur

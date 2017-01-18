05385370a385cc91da71a939c516c0e1fab5058e
orhan78 <orhan.yilmaz1978@gmail.com>
2017-01-18 10:49:33 +0200

    Java dilinde sayi okuyup ekrana basar
    Orhan Yılmaz tarafından yazılmıştır.

---
+++ sayiokuma.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class Hello {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println ("Bir Sayı Giriniz:  ");
+		int i = sc.nextInt();
+		System.out.println ("Girdiğiniz Sayı: "+ i);
+
+	}
+
+}

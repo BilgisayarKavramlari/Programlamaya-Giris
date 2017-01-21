49b226738ccedc8b3f445370bff5ede237c874f0
Bora Ikizoglu <boraikizoglu@gmail.com>
2017-01-21 04:56:37 +0200

    C++ Programlama Dilinde Sayı Okuma

---
+++ SayiOkuma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	int a;
+	cout << "Bir Sayi Giriniz:\n";
+	cin >> a;
+	string text = "Girdiginiz Sayi ";
+	cout << text << a;
+	return 0;
+
+}

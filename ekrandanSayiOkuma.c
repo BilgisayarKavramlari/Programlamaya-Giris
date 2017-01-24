0aec1977b91fc8dab807b2aaa58cc1f73eab74c2
bassoprofondo <oguzkaanbulut86@gmail.com>
2017-01-24 23:27:05 +0300

    Konsol ekranından sayı okuyan c kodu
    Oğuz Kaan BULUT tarafından yazıldı.

---
+++ ekrandanSayiOkuma.c
@@ -0,0 +1,17 @@
+//Created by Oğuz Kaan BULUT.
+//All rights reserved.
+
+#include <stdio.h>
+#include <conio.h>
+int main() {
+	
+	int a;
+	
+	printf("Ekrana basilacak sayi : ");
+	scanf("%d",&a);
+	printf("Girdiginiz sayi : %d",a);
+    
+	getch();
+    
+	return 0;
+}

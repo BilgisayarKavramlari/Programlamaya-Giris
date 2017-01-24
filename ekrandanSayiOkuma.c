848c1cc8a6b3bacedd851d93901b8ac1f28148c0
bassoprofondo <oguzkaanbulut86@gmail.com>
2017-01-24 22:21:43 +0300

    Konsol ekranından okunan sayıyı basan c kodu

---
+++ ekrandanSayiOkuma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+	
+	int a;
+	
+	printf("Ekrana basilacak sayi : ");
+	scanf("%d",&a);
+	printf("Girdiginiz sayi : %d",a);
+    
+	getch();
+    
+	return 0;
+}

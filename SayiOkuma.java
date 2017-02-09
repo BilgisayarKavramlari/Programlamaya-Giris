49dabb79a49c4f7535391ba2e77becd9f9465f2f
Göktuğ Cengiz <goktugcengiz7@gmail.com>
2017-02-09 13:02:53 +0200

    Java dilinde ekrandan sayı okuma
    Bu kod Göktuğ Cengiz tarafından yazılmıştır

---
+++ SayiOkuma.java
@@ -0,0 +1,11 @@
+package bilgisayarkavramlari;
+import java.util.Scanner;
+public class bilgisayarkavramlari {
+    public static void main(String[] args) {
+        Scanner sayiGir = new Scanner(System.in);
+        System.out.print("Bir sayi giriniz: ");
+        int sayi = sayiGir.nextInt();
+        System.out.println("Girdiğiniz sayı: "+sayi);
+    }
+    
+}

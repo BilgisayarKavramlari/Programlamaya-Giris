8fa0ee2c4b76cd7f8b2d6ba26a3e460579450482
Yasir ÇELİK <yasirc33@hotmail.com>
2017-05-16 23:30:48 +0300

    1den10aDongu.py
    Python3.x dilinde çözüm

---
+++ 1den10aDongu.py
@@ -0,0 +1,8 @@
+# 1den 10a kadar ardışık sayıları yazdırma
+
+i=1
+while i<=10:    # i sayımız 10'a küçük eşit olduğu sürece döngüyü döndürmeyi sağladık.
+    print(i)    # i sayımızı yazdırdık
+    i=i+1       # Yazdırma işleminden sonra 1 artırdık.
+    
+    

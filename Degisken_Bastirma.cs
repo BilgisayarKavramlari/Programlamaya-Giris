59b72cc587b7ba2b53171d3f6124928be18a8e7c
Mehmet Ali <mhmtaliongn@gmail.com>
2017-07-29 03:04:46 +0300

    C# Dilinde Degisken Bastirma
    Mehmet Ali tarafindan kodlanmistir

---
+++ Degisken_Bastirma.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Degisken_Bastirma
+{
+    public class Degisken_Bastirma
+    {
+        public static void Main(string[] args)
+        {
+            int deger = 5;
+            Console.Write("Değişkenin değeri = " + deger);
+            Console.ReadKey();
+        }
+    }
+}

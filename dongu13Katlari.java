3d6e82ed7da8fa0fa46f85f4dd23252198696268
Arif <arifgider2@gmail.com>
2017-08-27 14:34:42 +0300

    dongu13Katları.java

---
+++ dongu13Katlari.java
@@ -0,0 +1,12 @@
+package dongu13katlari;
+public class Dongu13Katlari { 
+    public static void main(String[] args) {
+    int a = 100;
+    int b = 13; 
+         for(int i = a; i>0; i--){
+              if(i%b == 0){
+                    System.out.println(i);
+             }
+         }
+    }  
+}

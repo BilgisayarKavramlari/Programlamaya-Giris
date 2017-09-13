0d6c919e4781055c5837ab9ee1975d69d3393217
Mehmet Ali <mhmtaliongn@gmail.com>
2017-09-13 21:21:22 +0300

    JAVA Dilinde Dizideki Aritmetik Ortalama
    Mehmet Ali tarafindan yazilmistir

---
+++ DizidekiOrtalama.java
@@ -0,0 +1,14 @@
+public class DizidekiOrtalama 
+{
+    public static void main(String[] args) 
+    {
+        int [] dizi = {1,2,3,4};
+        double  toplam = 0;
+        
+        for (int i = 0; i < dizi.length; i++) 
+        {
+            toplam += dizi[i]; 
+        }
+        System.out.println("Dizideki elemnaların geometrik ortalaması = " + toplam/dizi.length);
+    }   
+}

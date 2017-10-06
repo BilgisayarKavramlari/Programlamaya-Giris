2dc3ec5174c7c443bf73143d063121f0e07c7e55
puskuruk <puskuruk@gmail.com>
2017-10-06 23:30:17 +0300

    Create SayiOkuma.rb
    Ruby dilinde girdiğimiz sayıyı ekrana yazan program

---
+++ SayiOkuma.rb
@@ -0,0 +1,6 @@
+#Ruby dilinde girdiğimiz sayıyı ekrana basan bir kod yazacağız.
+puts "Bir sayi giriniz"
+#Ruby dinamik yapılı bir dil olduğu için string olduğunu belirtmemize gerek kalmıyor.
+girilen_sayi = gets.chomp
+#2 numaralı projemizde ekrana nasıl bastırdığımızı bir kodile göstermiştik.
+puts"Girdiğiniz sayi #{girilen_sayi}"

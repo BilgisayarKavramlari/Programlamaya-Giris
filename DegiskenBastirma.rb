b85ab5fcca9995f43ccaf0f9b564bdee8e55f27f
puskuruk <puskuruk@gmail.com>
2017-10-06 23:24:03 +0300

    Create DegiskenBastirma.rb
    Ruby dilinde ekrana bastırma örnekleri

---
+++ DegiskenBastirma.rb
@@ -0,0 +1,10 @@
+#Bu dosya Ruby dilinde hazırlanmıştır.
+#Bir değişkeni ekrana bastırmak:
+sayiyla_kirkbir = 41
+puts sayiyla_kirkbir
+#Bir string ile değişken bastırmak:
+puts "#{sayiyla_kirkbir} kere maşallah"
+#İçine string tanımlanmış bir değişkeni bastırmak:
+yaziyla_kirkbir = "kirkbir"
+#DRY prensibine uygun olmasa dakolay okunabilsin istedim.
+puts "#{yaziyla_kirkbir} kere maşallah"

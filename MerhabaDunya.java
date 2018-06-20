47f32a7f22675a83835e6b10cd0da4e2dfa34d55
voyula <40173603+voyula@users.noreply.github.com>
2018-06-20 11:36:42 +0300

    Update MerhabaDunya.java

--- MerhabaDunya.java
+++ MerhabaDunya.java
@@ -1,5 +1,5 @@
-public class MerhabaDuna{
-  public static void main(String args[]){
-    System.out.println("Merahba Dunya");
-  }
+public class MerhabaDunya {
+ public static void main(String args[]) {
+  System.out.println("Merhaba Dünya");
+ }
 }

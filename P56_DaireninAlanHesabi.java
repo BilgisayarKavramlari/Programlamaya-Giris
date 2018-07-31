4a9a027ff91fbad9b05278be10d9414bd7f4b18c
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2018-07-31 16:28:46 +0300

    Java ile DaireninAlanHesabi

---
+++ P56_DaireninAlanHesabi.java
@@ -0,0 +1,13 @@
+public class P56_DaireninAlanHesabi {
+
+	public static void main(String[] args) {
+		double yaricap = 5.75;
+		double cap = (yaricap) * 2;
+		double cevre = 2 * Math.PI * yaricap;
+		double alan = Math.PI * Math.pow(yaricap, 2);
+		System.out.println("Dairenin yaricapi : " + yaricap);
+		System.out.println("Dairenin capi : " + cap);
+		System.out.println("Dairenin cevresi : " + cevre);
+		System.out.println("Dairenin alani : " + alan);
+	}
+}

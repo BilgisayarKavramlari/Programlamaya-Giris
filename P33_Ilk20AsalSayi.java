b4f4fe6843845372fd12cc1ba159e30cb953fc80
Kutay Büyükkörükçü <kutaybuyukkorukcu@gmail.com>
2018-07-31 15:44:46 +0300

    Update P33_Ilk20AsalSayi.java

--- P33_Ilk20AsalSayi.java
+++ P33_Ilk20AsalSayi.java
@@ -1,18 +1,16 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class P33_Ilk20AsalSayi {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		ArrayList<Integer> ar = new ArrayList<Integer>();
-		System.out.println("Kacinci sayiya kadar mersenne sayisi gormek istiyorsunuz? ");
+		System.out.println("Kac tane asal sayi gormek istiyorsunuz? ");
 		int sayi = input.nextInt();
 		int count = 0;
 		int num = 2;
 		while (count != sayi) {
 			boolean asal = true;
-			for (int i = 2; i < Math.sqrt(num); i++) {
+			for (int i = 2; i < num / 2 + 1; i++) {
 				if (num % i == 0) {
 					asal = false;
 					break;

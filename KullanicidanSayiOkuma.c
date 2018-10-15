f8669ad95a68c32b43c8cd4ae72dc7e18357e6a2
Reitnos <42033267+Reitnos@users.noreply.github.com>
2018-10-15 10:51:00 +0300

    Add files via upload
    a code which asks to users write a number with printf , and get number with scanf then feedback to user again with printf.

---
+++ KullanicidanSayiOkuma.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int sayi;
+printf("lutfen bir sayi giriniz : \n ");
+
+scanf("%d",sayi);
+
+printf("girdiginiz sayi : %d \n",sayi);
+
+
+
+
+
+system("pause");
+}

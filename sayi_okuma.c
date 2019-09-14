aedf05136e2d960d9612ce98681db3d157fd2f69
neksirdi <47148991+neksirdi@users.noreply.github.com>
2019-09-14 22:33:04 +0300

    Create sayi_okuma.c

---
+++ sayi_okuma.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a; //14 September 2019 by neksirdi
+    printf("Lütfen bir sayı giriniz:\n");
+    scanf("%i", &a);
+    printf("Girdiğiniz sayı: %i\n", a);
+    return 0;
+}

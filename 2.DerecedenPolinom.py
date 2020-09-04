1ad0eb74d92f4272a7186fc2401f765524291380
artogahr <ertogrulgehremanli@gmail.com>
2020-09-04 14:23:14 +0300

    Duplicate dosyalar silindi ve isimler tutarli hale getirildi

---
+++ 2.DerecedenPolinom.py
@@ -0,0 +1,6 @@
+a= int(input("[a^2 + b^2 + 3c] polinomu için a Değerini Giriniz !"))
+b= int(input("[a^2 + b^2 + 3c] polinomu için b Değerini Giriniz !"))
+c= int(input("[a^2 + b^2 + 3c] polinomu için c Değerini Giriniz !"))
+
+sonuc = a*a+b*b+3*c
+print(sonuc)

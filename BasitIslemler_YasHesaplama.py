5f523caecd90230a5cd5860416e9836a62bb16c9
obligar <45253487+obligar@users.noreply.github.com>
2021-11-28 03:32:14 +0300

    Python Dilinde Yaş Hesaplama
    Python3.x dilinde çözüm

---
+++ BasitIslemler_YasHesaplama.py
@@ -0,0 +1,22 @@
+# Dogum tarihi hesaplama
+yil = input("Hangi yıldayız?: ")
+yas = input("Yaşınız kaç?: ")
+
+yil = int(yil)
+yas = int(yas)
+
+print("Bugünün tarihi:", yil)
+print("Yaşınız:", yas)
+print("Doğum yılınız:", yil - yas)
+
+
+# Yas hesaplama
+yil = input("Hangi yıldayız?: ")
+dogum_yili = input("Doğum yılınızı giriniz: ")
+
+yil = int(yil)
+dogum_yili = int(dogum_yili)
+
+print("Bugünün tarihi:", yil)
+print("Doğum yılınız:", dogum_yili)
+print("Yaşınız:", yil - dogum_yili)

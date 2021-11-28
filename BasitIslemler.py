bbbe688cc96a4291e0781595127f6faa6f816e89
obligar <45253487+obligar@users.noreply.github.com>
2021-11-28 03:05:22 +0300

    Python Dilinde Basit İşlemler
    Python3.x dilinde çözüm

---
+++ BasitIslemler.py
@@ -0,0 +1,11 @@
+a = input("Lutfen birinci sayiyi giriniz: ")
+b = input("Lutfen ikinci sayiyi giriniz: ")
+
+a = int(a)
+b = int(b)
+
+print("toplam:", a + b)
+print("fark:", a - b)
+print("carpim", a * b)
+print("bolum", a / b)
+print("kalan", a % b)

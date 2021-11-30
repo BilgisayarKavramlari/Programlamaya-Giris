02926cdcfc2fa6665826a990019c811e406235da
obligar <45253487+obligar@users.noreply.github.com>
2021-12-01 00:00:02 +0300

    Yıldızlardan Eşkenar Dörtgeni Ekrana Basan Kod
    Python3.x dilinde çözüm

---
+++ YildizlardanEskenarDortgen.py
@@ -0,0 +1,7 @@
+n = int(input("Bir sayı giriniz: "))
+
+for i in range(n):              # satır sayısı
+    for j in range(n):          # sütun sayısı
+        print("*", end=" ")
+    
+    print()

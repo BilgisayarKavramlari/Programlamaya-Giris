a755970861a5a621abece99e7946e5d914385c98
obligar <45253487+obligar@users.noreply.github.com>
2021-12-04 17:20:01 +0300

    OBEB Bulan Kod
    Python3.x dilinde çözüm

---
+++ OBEB.py
@@ -0,0 +1,13 @@
+sayi1 = int(input("Birinci sayıyı giriniz: "))
+sayi2 = int(input("İkinci sayıyı giriniz: "))
+
+if sayi1 > sayi2:
+    kucuk_sayi = sayi2
+else:
+    kucuk_sayi = sayi1
+
+for i in range(1, kucuk_sayi+1):
+    if sayi1 % i == 0 and sayi2 % i == 0:
+        obeb = i
+
+print("OBEB:", obeb)

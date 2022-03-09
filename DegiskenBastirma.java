1c23a13d3a1fa69fbe8fa343b5617bda509a8e3a
Enes006 <74904094+Enes006@users.noreply.github.com>
2022-03-09 18:50:20 +0300

    DegiskenBastirma.java

---
+++ DegiskenBastirma.java
@@ -0,0 +1,12 @@
+package javatutorial;
+
+public class Deneme {
+
+    public static void main(String[] args)
+    {
+        
+       int sayi = 5;
+       System.out.println("Değişken değeri: "+sayi);
+        
+    }
+}

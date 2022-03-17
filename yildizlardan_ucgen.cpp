30f3bb7fe39831bd42eb2a19c36a0cdbb6cf43b7
880066 <60427423+880066@users.noreply.github.com>
2022-03-17 05:55:21 +0300

    Update yildizlardan_ucgen.cpp

--- yildizlardan_ucgen.cpp
+++ yildizlardan_ucgen.cpp
@@ -1,4 +1,6 @@
+
 #include <stdio.h>
+#include <typeinfo>
 
 
 void ucgen_ciz(int *);
@@ -7,27 +9,32 @@ int main (void)
 {
 
 	int *sayi = new int;
-	
+
 	printf("Ikizkenar dik ucgenin esit olan kenarlarindan birinin uzunlugunu giriniz...\n");
 	scanf("%d",sayi);
 	
+	
+    if(*sayi < 2 || typeid(*sayi).name() != typeid(5).name() ){
+        printf("Lutfen 2'den buyuk ve bir tam sayi degeri giriniz.");
+        return 0;
+    }
+
 	ucgen_ciz(sayi);
-	
+
 	delete sayi;
-	
+
 	return 0;
 }
 
 void ucgen_ciz(int *ptr)
 {
-	
-	
 	for(int i = 1 ; i<=*ptr; i++){
-	
+
 		for(int j = 1 ; j<=i ; j++)
 			printf("*");
-		
+
 		printf("\n");
 	}
-	
+
+    
 }

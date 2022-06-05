bc0d404759c18e0d8c233b2e2b11c794221d336a
obligar <45253487+obligar@users.noreply.github.com>
2022-06-05 22:50:08 +0300

    Dairenin Alanını Hesaplayan Kod
    Python3.x dilinde çözüm

---
+++ DaireninAlanınıHesaplama.py
@@ -0,0 +1,17 @@
+r = 5.75 # yarıçapı
+pi = 3.14
+
+alan = pi * r**2
+cevre = 2 * pi * r
+
+print("\nDairenin çapı, alanı ve çevresi:\n\
+Çap: {}\n\
+Alan: {:.2f}\n\
+Çevre: {}".format(2*r, alan, cevre))
+
+"""
+Dairenin çapı, alanı ve çevresi:
+Çap: 11.5
+Alan: 103.82
+Çevre: 36.11
+"""

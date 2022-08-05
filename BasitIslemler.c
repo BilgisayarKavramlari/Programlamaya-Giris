7340f80356a14867f25324a8dc24c7e6c6bec2a2
beyzaalcin <110589657+beyzaalcin@users.noreply.github.com>
2022-08-05 10:13:35 +0300

    C dilinde Basit İşlemler kodu
    Beyza Nur ALÇIN

---
+++ BasitIslemler.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+  int a,b;
+  printf("lütfen bir sayı giriniz");
+  scanf("%d", &a);
+  printf("lütfen bir sayı giriniz");
+  scanf("%d", &b);
+  printf("%d", a+b);
+  printf("%d", a-b);
+  printf("%d", a*b);
+  printf("%d", a/b);
+  printf("%d", a%b);
+  return 0;
+}

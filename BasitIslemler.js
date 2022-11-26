d4d9739ca42f350bf1bdd139a6e770a7412f5fc3
Enes006 <74904094+Enes006@users.noreply.github.com>
2022-11-26 13:15:56 +0300

    BasitIslemler.js

---
+++ BasitIslemler.js
@@ -0,0 +1,17 @@
+let number1 = prompt("Lutfen birinci sayiyi giriniz: ")
+let number2 = prompt("Lutfen ikinci sayiyi giriniz: ")
+
+x = parseInt(number1)
+y = parseInt(number2)
+
+let toplam = (x)+(y)
+let fark = (x-y)
+let carpim = (x*y)
+let bolum = (x/y)
+let kalan = (x%y)
+
+console.log("Toplam: "+toplam)
+console.log("Fark: "+fark)
+console.log("Carpim: "+carpim)
+console.log("Bolum: "+bolum)
+console.log("Kalan: "+kalan)

d85ddeacf45b966de6eeb8ab92b9f001d47b34c7
Borauney <74118069+Borauney@users.noreply.github.com>
2023-07-29 09:13:30 +0300

    Create merhabadunya.cs

---
+++ merhabadunya.cs
@@ -0,0 +1,9 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Çıkış Yapmak için bir tuşa basınız.");
+            Console.ReadKey();
+        }
+    }

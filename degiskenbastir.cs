c9d936d5c1ac7bbb58ae299a610798a9e39b274e
Borauney <74118069+Borauney@users.noreply.github.com>
2023-07-29 09:55:32 +0300

    Create degiskenbastir.cs

---
+++ degiskenbastir.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace degiskenbastir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine("Number: " +number, number);
+        }
+    }
+}

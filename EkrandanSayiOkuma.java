e48efa5837fb54365f2fa01c922d75d8e4411a48
ibrahim <74904094+Enes006@users.noreply.github.com>
2023-09-07 21:43:50 +0300

    Create EkrandanSayiOkuma.java

---
+++ EkrandanSayiOkuma.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Lütfen bir sayı girin : ");
+        int number = scanner.nextInt();
+
+        System.out.println("Girdiğiniz sayı : " + number);
+    }
+}

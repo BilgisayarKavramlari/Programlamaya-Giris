78e118ead897112e8ffca200e1d823fc091b6be8
Yufuscuk <148553166+Yufuscuk@users.noreply.github.com>
2023-10-20 17:49:15 +0300

    Update MerhabaDunya.java

--- MerhabaDunya.java
+++ MerhabaDunya.java
@@ -1,5 +1,5 @@
-public class MerhabaDuna{
+public class MerhabaDunya{
   public static void main(String args[]){
-    System.out.println("Merahba Dunya");
+    System.out.println("Merhaba Dunya");
   }
 }
